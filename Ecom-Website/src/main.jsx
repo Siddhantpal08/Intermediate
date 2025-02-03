d95b90b60cbb2acef6c58101962c5721969adf81
Siddhant Pal <palsiddhant3@gmail.com>
2025-02-03 15:02:22 +0530

    debug

--- Ecom-Website/src/main.jsx
+++ Ecom-Website/src/main.jsx
@@ -6,7 +6,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import User from './components/User/User.jsx'
-import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+import Github, { githubInfoLoader } from './components/Github/github.jsx'
 import './App.css'
 
 
